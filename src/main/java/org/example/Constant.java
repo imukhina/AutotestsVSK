d4456a9abf9f5fa1d42a5b502a8f9486e7ce6872
Александр Ульяницкий <alex.backend.dev@gmail.com>
2025-02-10 19:17:51 +0300

    Добавил изменения для асертов и общего метода

--- src/main/java/org/example/Constant.java
+++ src/main/java/org/example/Constant.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Constant {
-    public static final String expectedPageTitle= "Automation Exercise";
+    public static final String actualTitle= "Automation Exercise";
     public static final String enterRassword1 = "qwerty123";
     ///public static final String expectedPageTitle= "Automation Exercise";
     ///public static final String expectedPageTitle= "Automation Exercise";
